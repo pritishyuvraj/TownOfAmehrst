9a68e6fbcc64657abee90d3e342767db57c632cb
pritishyuvraj <pritish.yuvi@gmail.com>
2019-02-11 14:11:58 -0500

    Tab Layouts done

--- app/src/main/java/com/example/townofamherst/MainActivity.java
+++ app/src/main/java/com/example/townofamherst/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.townofamherst;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        adapter = new TabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TopAttractionsPage(), getString(R.string.top_attractions));
+        adapter.addFragment(new RestaurantsPage(), getString(R.string.restaurants));
+        adapter.addFragment(new PublicPlacesPage(), getString(R.string.public_places));
+        adapter.addFragment(new EventsPage(), getString(R.string.events_page));
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
